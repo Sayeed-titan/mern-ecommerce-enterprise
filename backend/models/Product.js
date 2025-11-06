5419f81e39399ee70744a991a3945aa7bb59582a
Sayeed-titan <kaziabu.sayeed29@gmail.com>
2025-11-06 22:35:30 +0600

    Update product schema

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -8,8 +8,9 @@ const variantSchema = new mongoose.Schema({
   },
   sku: {
     type: String,
-    required: true,
+    required: false,
     unique: true,
+    sparse: true,
   },
   price: {
     type: Number,
